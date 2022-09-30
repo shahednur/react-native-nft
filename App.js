2b8546c6b99ee65fd4116d0868b5b4438231931c
shahednur <shahednurctg@gmail.com>
2022-10-01 00:43:38 +0600

    initial commit

---
+++ App.js
@@ -0,0 +1,36 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, DefaultTheme  } from '@react-navigation/native';
+
+import { HomeScreen, DetailScreen } from './screens';
+import { useFonts } from 'expo-font';
+
+const Stack = createStackNavigator();
+const theme={
+  ...DefaultTheme,
+  colors:{
+    ...DefaultTheme.colors,
+    background: 'transparent'
+  }
+}
+
+export default function App() {
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+
+  });
+
+  if(!loaded) return null;
+  
+  return (
+    <NavigationContainer theme={theme }>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
